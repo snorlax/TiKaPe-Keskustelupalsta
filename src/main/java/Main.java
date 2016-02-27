6c517db71b2c6a26e2cf7e91f44a585bacdf44ca
MacMac1 <mikko.p.makela@helsinki.fi>
2016-02-27 17:18:38 +0200

    Mainia siivottu että voin alkaa lisäämään spark juttuja. Nyt kaikki mainin jutut metodeina

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,42 +9,43 @@ public class Main {
         c.testaaYhteys();
         c.foreignKeytPaalle();
         System.out.println("");
-        
-//      Viestien lisääminen nykymallin mukaan:
-//        ViestiDao vd = new ViestiDao(c);
-//        boolean tulos = vd.lisaaVastausviesti(3, "testiketjun kolme avausviesti", "henri P");
-//        if (tulos) {
-//            System.out.println("Viestin lisäys onnistui!\n");
-//        } else {
-//            System.out.println("Viestin lisäys ei onnistunut :( \n");
-//        }
-        
-        ArrayList<Alue> alueet = c.queryAndCollect("SELECT * FROM Alue;", new AlueCollector());
-        ArrayList<Ketju> ketjut = c.queryAndCollect("SELECT * FROM Ketju;", new KetjuCollector());
+          
+        c.suljeYhteys();
+        System.out.println("\n\nYhteys suljettu.");
+    }
+    
+    public void tulostaKaikkiViestit(DBContacter c) {
         ArrayList<Viesti> viestit = c.queryAndCollect("SELECT * FROM Viesti;", new ViestiCollector());
-
-        System.out.println("Kaikki alueet");
-        for (Alue a : alueet) {
-            System.out.println(a);
-        }
-
-        System.out.println("");
-
-        System.out.println("Kaikki ketjut");
-        for (Ketju k : ketjut) {
-            System.out.println(k);
-        }
-
-        System.out.println("");
-
-        System.out.println("");
-
         System.out.println("Kaikki viestit:");
         for (Viesti s : viestit) {
             System.out.println(s);
         }
-        c.suljeYhteys();
-        System.out.println("\n\nYhteys suljettu.");
+    }
+    
+    public void tulostaKaikkiKetjut(DBContacter c) {
+        ArrayList<Ketju> ketjut = c.queryAndCollect("SELECT * FROM Ketju;", new KetjuCollector());
+        System.out.println("Kaikki ketjut");
+        for (Ketju k : ketjut) {
+            System.out.println(k);
+        }
+    }
+    
+    public void tulostaKaikkiAlueet(DBContacter c) {
+        ArrayList<Alue> alueet = c.queryAndCollect("SELECT * FROM Alue;", new AlueCollector());
+        System.out.println("Kaikki alueet");
+        for (Alue a : alueet) {
+            System.out.println(a);
+        }
+    }
+    
+    public void uusiViesti(DBContacter c) {
+        ViestiDao vd = new ViestiDao(c);
+        boolean tulos = vd.lisaaVastausviesti(3, "testiketjun kolme avausviesti", "henri P");
+        if (tulos) {
+            System.out.println("Viestin lisäys onnistui!\n");
+        } else {
+            System.out.println("Viestin lisäys ei onnistunut :( \n");
+        }
     }
 
 }
